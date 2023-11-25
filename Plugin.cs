f64d30f8078a79ac9a048349d0bddd4489e7866e
Electrostats <Super_justy@hotmail.com>
2023-11-25 01:53:41 -0500

    Initial commit

--- Plugin.cs
+++ Plugin.cs
@@ -2,27 +2,28 @@
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using System.Collections.Generic;
 using System.IO;
+using TootTallyCore.Graphics;
 using TootTallyCore.Utils.TootTallyModules;
 using TootTallySettings;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace TootTally.ModuleTemplate
+namespace TootTallyKeyOverlay
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    [BepInDependency("TootTallyCore", BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("TootTallySettings", BepInDependency.DependencyFlags.HardDependency)]
     public class Plugin : BaseUnityPlugin, ITootTallyModule
     {
         public static Plugin Instance;
 
-        private const string CONFIG_NAME = "ModuleTemplate.cfg";
         private Harmony _harmony;
         public ConfigEntry<bool> ModuleConfigEnabled { get; set; }
         public bool IsConfigInitialized { get; set; }
 
         //Change this name to whatever you want
-        public string Name { get => PluginInfo.PLUGIN_NAME; set => Name = value; }
+        public string Name { get => "KeyOverlay"; set => Name = value; }
 
         public static TootTallySettingPage settingPage;
 
@@ -41,27 +42,14 @@ namespace TootTally.ModuleTemplate
         private void TryInitialize()
         {
             // Bind to the TTModules Config for TootTally
-            ModuleConfigEnabled = TootTallyCore.Plugin.Instance.Config.Bind("Modules", "<insert module name here>", true, "<insert module description here>");
+            ModuleConfigEnabled = TootTallyCore.Plugin.Instance.Config.Bind("Modules", "KeyOverlay", true, "Enabled visual display for key strokes during a song.");
             TootTallyModuleManager.AddModule(this);
             TootTallySettings.Plugin.Instance.AddModuleToSettingPage(this);
         }
 
         public void LoadModule()
         {
-            string configPath = Path.Combine(Paths.BepInExRootPath, "config/");
-            ConfigFile config = new ConfigFile(configPath + CONFIG_NAME, true) { SaveOnConfigSet = true };
-            // Set your config here by binding them to the related ConfigEntry
-            // Example:
-            // Unlimited = config.Bind(CONFIG_FIELD, "Unlimited", DEFAULT_UNLISETTING)
-
-            settingPage = TootTallySettingsManager.AddNewPage("ModulePageName", "HeaderText", 40f, new Color(0,0,0,0));
-            if (settingPage != null) {
-                // Use TootTallySettingPage functions to add your objects to TootTallySetting
-                // Example:
-                // page.AddToggle(name, option.Unlimited);
-            }
-
-            _harmony.PatchAll(typeof(ModuleTemplatePatches));
+            _harmony.PatchAll(typeof(KeyOverlayPatches));
             LogInfo($"Module loaded!");
         }
 
@@ -72,13 +60,84 @@ namespace TootTally.ModuleTemplate
             LogInfo($"Module unloaded!");
         }
 
-        public static class ModuleTemplatePatches
+        public static class KeyOverlayPatches
         {
-            // Apply your Trombone Champ patches here
-        }
+            private static Dictionary<KeyCode, SingleKey> _keyPressedDict;
+            private static CustomButton _keyPrefab;
+            private static GameObject _keyOverlayUI;
 
-        // Add your ConfigEntry objects that define your configs
-        // Example:
-        // public ConfigEntry<bool> Unlimited { get; set; }
+            [HarmonyPatch(typeof(LevelSelectController), nameof(LevelSelectController.Start))]
+            [HarmonyPostfix]
+            public static void SetKeyOverlayPrefab(LevelSelectController __instance)
+            {
+                if (_keyPrefab != null) return;
+                var tempObj = GameObjectFactory.CreateCustomButton(__instance.bgshape.transform, Vector2.zero, new Vector2(50, 50), "test", "tempObj"); //idk where to put the tempObj just put it somewhere random lmfao
+                _keyPrefab = GameObject.Instantiate(tempObj);
+                _keyPrefab.gameObject.name = "KeyOverlayPrefab";
+                _keyPrefab.GetComponent<RectTransform>().sizeDelta = Vector2.one * 30;
+                _keyPrefab.textHolder.fontSize = 10;
+                var keyText = GameObject.Instantiate(_keyPrefab.transform.Find("Text"), _keyPrefab.transform);
+                keyText.name = "KeyText";
+                keyText.GetComponent<Text>().color = new Color(_keyPrefab.textHolder.color.r, _keyPrefab.textHolder.color.g, _keyPrefab.textHolder.color.b, .45f);
+                keyText.GetComponent<Text>().fontSize = 30;
+                GameObject.DestroyImmediate(tempObj.gameObject);
+                GameObject.DontDestroyOnLoad(_keyPrefab);
+            }
+
+
+            [HarmonyPatch(typeof(GameController), nameof(GameController.Start))]
+            [HarmonyPostfix]
+            public static void OnGameControllerStartSetupOverlay(GameController __instance)
+            {
+                if (__instance.freeplay) return;
+
+                var uiCanvas = GameObject.Find("GameplayCanvas/UIHolder");
+
+                _keyOverlayUI = GameObject.Instantiate(_keyPrefab, uiCanvas.transform).gameObject;
+                GameObject.DestroyImmediate(_keyOverlayUI.transform.Find("Text"));
+                GameObject.DestroyImmediate(_keyOverlayUI.GetComponent<UnityEngine.UI.Image>());
+                var rectTransform = _keyOverlayUI.GetComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(30, 180);
+                rectTransform.anchoredPosition = new Vector2(17, -90);
+                var verticalLayout = _keyOverlayUI.AddComponent<VerticalLayoutGroup>();
+                verticalLayout.childAlignment = TextAnchor.MiddleCenter;
+                verticalLayout.childScaleWidth = verticalLayout.childScaleHeight = false;
+                verticalLayout.childForceExpandWidth = verticalLayout.childForceExpandHeight = false;
+                verticalLayout.childControlWidth = verticalLayout.childControlHeight = false;
+
+                _keyPressedDict = new Dictionary<KeyCode, SingleKey>();
+            }
+
+            [HarmonyPatch(typeof(GameController), nameof(GameController.Update))]
+            [HarmonyPostfix]
+            public static void OnUpdateDetectKeyPressed(GameController __instance, List<KeyCode> ___toot_keys)
+            {
+                if (__instance.freeplay || _keyPressedDict == null) return;
+                ___toot_keys.ForEach(key =>
+                {
+                    if (Input.GetKey(key))
+                    {
+                        if (!_keyPressedDict.ContainsKey(key))
+                        {
+                            //put it inside to not trigger the else statement when already at 6 keys
+                            if (_keyPressedDict.Count < 6)
+                            {
+                                var newKeyGameObject = GameObject.Instantiate(_keyPrefab, _keyOverlayUI.transform);
+                                newKeyGameObject.transform.Find("KeyText").GetComponent<Text>().text = key.ToString();
+                                newKeyGameObject.name = $"KeyOverlay{key}";
+                                _keyPressedDict.Add(key, new SingleKey(newKeyGameObject));
+                                _keyPressedDict[key].OnKeyPress();
+                                Plugin.LogInfo($"New key pressed, adding {key} to overlay.");
+                            }
+                        }
+                        else if (!_keyPressedDict[key].isPressed)
+                            _keyPressedDict[key].OnKeyPress();
+                    }
+                    else if (_keyPressedDict.ContainsKey(key) && _keyPressedDict[key].isPressed)
+                        _keyPressedDict[key].OnKeyRelease();
+                });
+            }
+
+        }
     }
 }
---
+++ SingleKey.cs
@@ -0,0 +1,30 @@
+﻿using TootTallyCore.Graphics;
+using UnityEngine.EventSystems;
+
+namespace TootTallyKeyOverlay
+{
+    public class SingleKey
+    {
+        private CustomButton _gameObject;
+        public bool isPressed;
+        private int _pressCount; 
+
+        public SingleKey(CustomButton gameObject)
+        {
+            _gameObject = gameObject;
+        }
+
+        public void OnKeyPress()
+        {
+            _gameObject.button.OnPointerEnter(new PointerEventData(EventSystem.current));
+            _pressCount++;
+            _gameObject.textHolder.text = _pressCount.ToString();
+            isPressed = true;
+        }
+        public void OnKeyRelease()
+        {
+            _gameObject.button.OnPointerExit(new PointerEventData(EventSystem.current));
+            isPressed = false;
+        }
+    }
+}
